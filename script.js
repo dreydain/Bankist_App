addd1b153578dae5edae3adbcc0ab4260b39de6c
April Hughes <dreydain@gmail.com>
2022-09-23 14:18:58 -0700

    Implemented Loan functionality

--- script.js
+++ script.js
@@ -175,6 +175,25 @@ btnTransfer.addEventListener('click', function (e) {
 	// console.log(amount, receiverAcc);
 });
 
+btnLoan.addEventListener('click', function (e) {
+	e.preventDefault();
+
+	const amount = Number(inputLoanAmount.value);
+
+	if (
+		amount > 0 &&
+		currentAccount.movements.some((mov) => mov >= amount * 0.1)
+	) {
+		//Add Movement
+		currentAccount.movements.push(amount);
+
+		//Update UI
+		updateUI(currentAccount);
+	}
+
+	inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
 	e.preventDefault();
 
