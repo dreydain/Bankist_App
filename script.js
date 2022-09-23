c50e632443c9c6d484246015aa6ce8f4f406894a
April Hughes <dreydain@gmail.com>
2022-09-23 14:01:23 -0700

    Created delete account function

--- script.js
+++ script.js
@@ -174,3 +174,26 @@ btnTransfer.addEventListener('click', function (e) {
 
 	// console.log(amount, receiverAcc);
 });
+
+btnClose.addEventListener('click', function (e) {
+	e.preventDefault();
+
+	if (
+		inputCloseUsername.value === currentAccount.username &&
+		Number(inputClosePin.value) === currentAccount.pin
+	) {
+		const index = accounts.findIndex(
+			(acc) => acc.username === currentAccount.username
+		);
+
+		console.log(index);
+
+		// Delete Account
+		accounts.splice(index, 1);
+
+		// Hide UI
+		containerApp.style.opacity = 0;
+	}
+
+	inputCloseUsername.value = inputClosePin.value = '';
+});
